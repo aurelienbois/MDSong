a06d572e643fde9736a94a432cfcc4fb739a02cf
Alex-lyon <tanokasse18@gmail.com>
2024-01-02 18:14:34 +0100

    Ajout du titre et du nom de l'artiste dans le tableau de sons et dans le composant oneSong

--- src/components/OneSong.js
+++ src/components/OneSong.js
@@ -9,7 +9,17 @@ function OneSong(song) {
   return (
     <div className='one-music py-3 gap-4' onClick={()=>{changeCurrentSong(song.song); setPlay(true)}}>
         <img className='' alt='music' src={process.env.PUBLIC_URL+"/dossiers_chansons/"+song.song.cover}></img>
-        
+        <div>
+            <h4 className={(currentSong.title === song.song.title)?'text-info':''}>{song.song.title}</h4>
+            <h6 className=''>{song.song.artist}</h6>
+        </div>
+        <button className='btn border-0 ms-auto p-0'>
+          {
+            (currentSong.title === song.song.title)?
+            <i className="fa-solid fa-chart-simple fa-beat text-info"></i>
+            :<i className="fa-solid fa-circle-play"></i>
+          }
+        </button>
     </div>
   )
 }
--- src/sons.js
+++ src/sons.js
@@ -1,51 +1,75 @@
 const sons = [
     {
         song: "alix/alix_song.mp3",
-        cover: "alix/alix_cover.jpeg"
+        cover: "alix/alix_cover.jpeg",
+        title: "Sous le vent",
+        artist: "Alix"
     },
     {
         song: "batiste/batiste_song.mp3",
-        cover: "batiste/batiste_cover.jpeg"
+        cover: "batiste/batiste_cover.jpeg",
+        title: "Intro",
+        artist: "Batiste"
     },
     {
         song: "clara/clara_song.mp3",
-        cover: "clara/clara_cover.jpeg"
+        cover: "clara/clara_cover.jpeg",
+        title: "Girls just want to have fun",
+        artist: "Clara"
     },
     {
         song: "hugo/hugo_song.mp3",
-        cover: "hugo/hugo_cover.jpeg"
+        cover: "hugo/hugo_cover.jpeg",
+        title: "C'est chiant l'école",
+        artist: "Hugo"
     },
     {
         song: "jules/jules_song.mp3",
-        cover: "jules/jules_cover.jpeg"
+        cover: "jules/jules_cover.jpeg",
+        title: "Soon we'll be found",
+        artist: "Jules"
     },
     {
         song: "kentin/kentin_song.mp3",
-        cover: "kentin/kentin_cover.jpeg"
+        cover: "kentin/kentin_cover.jpeg",
+        title: "Beautiful Pain",
+        artist: "Kentin"
     },
     {
         song: "margaux/margaux_song.mp3",
-        cover: "margaux/margaux_cover.jpeg"
+        cover: "margaux/margaux_cover.jpeg",
+        title: "Make your own happiness",
+        artist: "Margaux"
     },
     {
         song: "marius/marius_song.mp3",
-        cover: "marius/marius_cover.jpeg"
+        cover: "marius/marius_cover.jpeg",
+        title: "Alive",
+        artist: "Marius"
     },
     {
         song: "sanson/sanson_song.mp3",
-        cover: "sanson/sanson_cover.jpeg"
+        cover: "sanson/sanson_cover.jpeg",
+        title: "Si j'avais su",
+        artist: "Sanson"
     },
     {
         song: "thoma/thoma_song.mp3",
-        cover: "thoma/thoma_cover.jpeg"
+        cover: "thoma/thoma_cover.jpeg",
+        title: "Bécane",
+        artist: "Thoma"
     },
     {
         song: "thoma_c/thomas_c_song.mp3",
-        cover: "thoma_c/thomas_c_cover.jpeg"
+        cover: "thoma_c/thomas_c_cover.jpeg",
+        title: "Imagine un monde",
+        artist: "Thomas C"
     },
     {
         song: "valentin/valentin_song.mp3",
-        cover: "valentin/valentin_cover.jpeg"
+        cover: "valentin/valentin_cover.jpeg",
+        title: "Fleurs du mal",
+        artist: "Valentin"
     }
 ]
 
