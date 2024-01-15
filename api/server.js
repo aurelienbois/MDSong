c17dbb5e712d0144eb68e4a088329f56ec45fe4a
Aurélien Bois <cci@aurelienbois.fr>
2024-01-15 23:22:44 +0100

    allow all origins

--- api/server.js
+++ api/server.js
@@ -3,6 +3,19 @@ const sons = require("./sons.js")
 
 const app = express()
 
+const cors = require("cors")
+app.use(cors())
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  )
+  next()
+})
+
+
 app.get("/songs", (req, res) => {
   res.json(sons) // Renvoyer toutes les chansons
 })
