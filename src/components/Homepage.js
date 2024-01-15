8e51de9ab8e44e8890e6b48f6c944d9084435122
Losn0r <losn0rw3b@gmail.com>
2024-01-15 11:09:45 +0100

    Correction du bug sur le composant Homepage.js

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -10,6 +10,20 @@ function Homepage() {
     })
   return (
     <div className='home position-relative'>
+      <section className='top-section p-3'>
+            <h1 className='m-0'>MDS B2 2023-2024</h1>
+            <p className='m-0 '>Ma playlist &nbsp;&nbsp;¤&nbsp;&nbsp; 12 sons &nbsp;&nbsp;¤&nbsp;&nbsp; 1 heure </p>
+        </section>
+        <section className='bottom-section p-3 pt-4'>
+            <button className='shuffle rounded shadow p-2'><i class="fa-solid fa-shuffle"></i>&nbsp;&nbsp; Aléatoire</button>
+            <div className='liste-music pt-3 pb-5'>
+                {
+                  songs?.map((song)=>(
+                    <OneSong song={song}></OneSong>
+                  ))
+                }
+            </div>
+        </section>
         <audio className='d-none' src={process.env.PUBLIC_URL+"/dossiers_chansons/"+currentSong?.song} controls autoPlay/>
         {
           // vérifier que le tableau de sons n'est pas vide avant d'afficher le player.
